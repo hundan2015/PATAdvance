a37cd9ee02321b82a3df8fe56d65df0c61334838
hundan2015 <sym12138@hotmail.com>
2023-06-16 21:21:05 +0800

    滑动窗口！

--- Acwing/154.cpp
+++ Acwing/154.cpp
@@ -2,8 +2,7 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-int main()
-{
+int main() {
     int n, k;
     cin >> n >> k;
     vector<int> nums(n);
@@ -11,24 +10,50 @@ int main()
         cin >> nums[i];
     }
     deque<int> dq;
+
     for (int i = 0; i < k; ++i) {
-        if (dq.front() < nums[i]) {
-            dq.push_front(nums[i]);
-        } else if (dq.back() > nums[i]) {
-            dq.clear();
-            dq.push_back(nums[i]);
-        }
-    }
-    for (int i = 1; i < n - k + 1; ++i) {
-        if (nums[i - 1] == dq.back()) {
+        while (!dq.empty() && dq.back() > nums[i]) {
             dq.pop_back();
         }
-        if (dq.front() < nums[i + k - 1]) {
-            dq.push_front(nums[i + k - 1]);
-        } else if (dq.back() > nums[i + k - 1]) {
-            dq.clear();
-            dq.push_back(nums[i + k - 1]);
+        dq.push_back(nums[i]);
+    }
+    cout << dq.front();
+    for (int i = 1; i < n - k + 1; ++i) {
+        if (nums[i - 1] == dq.front()) {
+            dq.pop_front();
         }
-        cout << dq.back() << " ";
+        while (!dq.empty() && dq.back() > nums[i + k - 1]) {
+            dq.pop_back();
+        }
+        dq.push_back(nums[i + k - 1]);
+        cout << " " << dq.front();
+    }
+    cout << endl;
+    // 单调队列的方法。
+    /**
+     * 单调队列只在队伍里保留可能成为最小值的元素。
+     * 假设一个元素把前面所有的都排除了，因为前面出现过的元素一定先于
+     * 这个元素调出窗口，因此他们不可能是最小值。只有后面进来的元素才可能在
+     * 后面的窗口中成为最小值的元素。
+     * 
+     * 还是需要解析一下这个方法的来源。
+     */
+    dq.clear();
+    for (int i = 0; i < k; ++i) {
+        while (!dq.empty() && dq.back() < nums[i]) {
+            dq.pop_back();
+        }
+        dq.push_back(nums[i]);
+    }
+    cout << dq.front();
+    for (int i = 1; i < n - k + 1; ++i) {
+        if (nums[i - 1] == dq.front()) {
+            dq.pop_front();
+        }
+        while (!dq.empty() && dq.back() < nums[i + k - 1]) {
+            dq.pop_back();
+        }
+        dq.push_back(nums[i + k - 1]);
+        cout << " " << dq.front();
     }
 }

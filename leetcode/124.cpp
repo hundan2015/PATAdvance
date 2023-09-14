e9aa54665c679efc05124a0432eb4189b7d9a2ae
hundan2015 <sym12138@hotmail.com>
2023-09-14 15:23:27 +0800

    fix glitch

--- leetcode/124.cpp
+++ leetcode/124.cpp
@@ -16,17 +16,18 @@ class Solution {
 
    public:
     int maxPathSum(TreeNode* root) {
-        dfs(root, 0);
+        dfs(root);
         return maxSolution;
     }
-    int dfs(TreeNode* root, int prevSum) {
+    // 返回经过这个节点，最长的路径长度
+    int dfs(TreeNode* root) {
         if (!root)
             return (-0x7fffffff - 1);
-        int sum = prevSum + root->val;
 
-        int leftMax = dfs(root->left, sum);
-        int rightMax = dfs(root->right, sum);
-
+        int leftMax = dfs(root->left);
+        int rightMax = dfs(root->right);
+        // 分别计算节点加上左最长长度，节点加上右最长长度，节点自己长度，节点加上两边最长谁最长。
+        // 这样就可以涵盖不同的路径了
         if (rightMax != (-0x7fffffff - 1) && leftMax != (-0x7fffffff - 1))
             maxSolution = max(
                 maxSolution,

e907f08a4a363962fff6bd6ae8911d940d526c28
hundan2015 <sym12138@hotmail.com>
2023-09-17 14:41:50 +0800

    9.17 daka

--- leetcode/42.cpp
+++ leetcode/42.cpp
@@ -1,21 +1,38 @@
+#include <bits/stdc++.h>
 #include <iostream>
-#include <vector>
 using namespace std;
+
+// 非常巧妙的递减单调栈问题
 class Solution {
    public:
     int trap(vector<int>& height) {
-        const int size = height.size();
-        vector<int> maxLeft(size, 0);
-        vector<int> maxRight(size, 0);
-        maxLeft[0] = 
-        for (int i = 1; i < size; ++i){
+        stack<int> stk;
+        int sum = 0;
+        for (int i = 0; i < height.size(); ++i) {
+            while (!stk.empty() && height[stk.top()] < height[i]) {
+                // 在这种情况下，就说明栈内有一个元素比当前小。
+                // 这个较小的元素作为当前需要填充部分的底部。
+                int downHill = height[stk.top()];
+                // 找到作为底的高度之后，我们要找作为墙的高度，因此pop
+                stk.pop();
+                // empty的时候，墙不存在了。因此不会积水，固向下面继续计算。
+                if (stk.empty())
+                    break;
+                // 作为墙的高度是比作为底的高度高的。
 
+                int distance = i - stk.top() - 1;
+                int minHeight = min(height[i], height[stk.top()]);
+                sum += distance * (minHeight - downHill);
+                // 然而我们并没有pop作为墙的高度的元素，因为这回合的墙，可能是下回合的底
+            }
+            stk.push(i);
         }
+        return sum;
     }
 };
 
 int main() {
     Solution sol;
-    vector<int> temp = {5, 4, 1, 2};
+    vector<int> temp = {0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};
     cout << sol.trap(temp);
 }
--- leetcode/502.cpp
+++ leetcode/502.cpp
@@ -1,5 +1,6 @@
 #include <algorithm>
 #include <iostream>
+#include <queue>
 #include <vector>
 using namespace std;
 class Solution {
@@ -18,9 +19,38 @@ class Solution {
             companys[i] = {profits[i], capital[i]};
         }
         sort(companys.begin(), companys.end(),
-             [](auto& a, auto& b) { return a.capital > b.capital; });
-        int sum = 0;
-        for (auto& i : companys) {
+             [](auto& a, auto& b) { return a.capital < b.capital; });
+        int sum = w;
+        int counter = 0;
+        auto cmp = [](auto& a, auto& b) { return a.profit < b.profit; };
+        priority_queue<company, vector<company>, decltype(cmp)> pq(cmp);
+        int cursor = 0;
+        while (cursor < companys.size() && counter < k) {
+            if (companys[cursor].capital <= sum) {
+                pq.push(companys[cursor]);
+                cursor++;
+            } else if (!pq.empty()) {
+                auto temp = pq.top();
+                pq.pop();
+                sum += temp.profit;
+                counter++;
+            } else {
+                break;
+            }
         }
+        while (!pq.empty() && counter < k) {
+            auto temp = pq.top();
+            pq.pop();
+            sum += temp.profit;
+            counter++;
+        }
+        return sum;
     }
 };
+
+int main() {
+    Solution sol;
+    vector<int> profits = {1, 2, 3};
+    vector<int> capital = {0, 1, 1};
+    cout << sol.findMaximizedCapital(2, 0, profits, capital);
+}
